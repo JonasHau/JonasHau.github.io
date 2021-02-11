7b9201a426b6d86f065f78eea8b6bc9d2d106e38
Frosted <35954302+JonasHau@users.noreply.github.com>
2021-02-11 12:14:23 +0100

    A few updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function tab(tab) {
   document.getElementById("homeMenu").style.display = "none"
   document.getElementById("skillMenu").style.display = "none"
+  document.getElementById("projectMenu").style.display = "none"
   document.getElementById("aboutMenu").style.display = "none"
   document.getElementById(tab).style.display = "inline-block"
 }
